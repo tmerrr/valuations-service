10bd0047c346d9a061ff818410d6f80da9428a9c
Tom Moir <trmoir9@gmail.com>
2024-06-15 11:34:41 +0100

    feat: finishes initial build of the Circuit Breaker class

--- src/__tests__/CircuitBreaker.test.ts
+++ src/__tests__/CircuitBreaker.test.ts
@@ -1,20 +1,48 @@
 import CircuitBreaker from '../lib/CircuitBreaker';
 
 describe('CircuitBreaker', () => {
+  beforeEach(() => {
+    vi.useRealTimers();
+  });
+
+  describe('state / isOpen / isClosed', () => {
+    it('should return "CLOSED" when the circuit is closed', () => {
+      const cb = new CircuitBreaker({
+        failureThresholdPercentage: 50,
+        resetTimeout: 1_000,
+      });
+      expect(cb.state).toBe('CLOSED');
+      expect(cb.isClosed()).toBe(true);
+      expect(cb.isOpen()).toBe(false);
+    });
+
+    it('should return "OPEN" when the circuit is open', () => {
+      const cb = new CircuitBreaker({
+        failureThresholdPercentage: 50,
+        resetTimeout: 1_000,
+      });
+      cb.overrideState('OPEN');
+      expect(cb.state).toBe('OPEN');
+      expect(cb.isClosed()).toBe(false);
+      expect(cb.isOpen()).toBe(true);
+    });
+  });
+
   describe('run', () => {
     it('should successfully return the value of the function on success', async () => {
       const cb = new CircuitBreaker({
-        errorThresholdPercentage: 50,
+        failureThresholdPercentage: 50,
         resetTimeout: 1_000,
       });
       const value = 1;
       const fn = vi.fn().mockResolvedValue(value);
       await expect(cb.run(fn)).resolves.toEqual(value);
+      expect(cb.state).toBe('CLOSED');
     });
   
     it('should return the value of "run" when successful, when a fallback is provided', async () => {
       const cb = new CircuitBreaker({
-        errorThresholdPercentage: 50,
+        failureThresholdPercentage: 50,
         resetTimeout: 1_000,
       });
       const value = 1;
@@ -25,13 +53,70 @@ describe('CircuitBreaker', () => {
   
     it('should return the value of the fallback when the primary function fails', async () => {
       const cb = new CircuitBreaker({
-        errorThresholdPercentage: 50,
+        failureThresholdPercentage: 50,
         resetTimeout: 1_000,
       });
       const value = 1;
       const fn = vi.fn().mockRejectedValue(new Error('test'));
       const fallback = vi.fn().mockResolvedValue(value);
       await expect(cb.run(fn, fallback)).resolves.toEqual(value);
+    });
+
+    it('should call the fallback fn when the circuit is open', async () => {
+      const cb = new CircuitBreaker({
+        failureThresholdPercentage: 50,
+        resetTimeout: 1_000,
+      });
+      cb.overrideState('OPEN');
+      const value = 1;
+      const primary = vi.fn();
+      const fallback = vi.fn().mockResolvedValue(value);
+      await expect(cb.run(primary, fallback)).resolves.toEqual(value);
+      expect(primary).not.toHaveBeenCalled();
+      expect(fallback).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an error when the circuit is open and no fallback is provided', async () => {
+      const cb = new CircuitBreaker({
+        failureThresholdPercentage: 50,
+        resetTimeout: 1_000,
+      });
+      cb.overrideState('OPEN');
+      const primary = vi.fn();
+      await expect(cb.run(primary)).rejects.toThrow('Circuit is open and no fallback provided');
+      expect(primary).not.toHaveBeenCalled();
+    });
+
+    it('remains in "CLOSED" state until the failure threshold is reached', async () => {
+      const cb = new CircuitBreaker({
+        failureThresholdPercentage: 49,
+        resetTimeout: 1_000,
+      });
+      const error = new Error('something went wrong!');
+      const fn = vi.fn()
+        .mockResolvedValueOnce(1)
+        .mockRejectedValueOnce(error);
+
+      await expect(cb.run(fn)).resolves.toEqual(1);
+      expect(cb.state).toBe('CLOSED');
+
+      await expect(cb.run(fn)).rejects.toThrow(error);
+      expect(cb.state).toBe('OPEN');
+    });
+
+    it('should reset the state of the circuit after the resetTimeout has passed', async () => {
+      vi.useFakeTimers();
+
+      const cb = new CircuitBreaker({
+        failureThresholdPercentage: 50,
+        resetTimeout: 2_000,
+      });
+
+      const fn = vi.fn().mockRejectedValue(new Error('something went wrong!'));
+      await expect(cb.run(fn)).rejects.toThrow();
+      expect(cb.state).toBe('OPEN');
+      vi.advanceTimersByTime(2_000);
+      expect(cb.state).toBe('CLOSED');
     });
   });
 });
--- src/lib/CircuitBreaker.ts
+++ src/lib/CircuitBreaker.ts
@@ -1,25 +1,97 @@
 export type CircuitBreakerConfig = {
-  errorThresholdPercentage: number;
+  failureThresholdPercentage: number;
   resetTimeout: number;
 };
+
+// simplified state so not using half open, but leaves open to extension
+export type CircuitBreakerState = 'CLOSED' | 'OPEN';
 
 type GenericFunction<T> = () => Promise<T>;
 
 export default class CircuitBreaker {
-  constructor(config: CircuitBreakerConfig) {}
+  private _state: CircuitBreakerState = 'CLOSED';
+
+  private totalCount = 0;
+
+  private failureCount = 0;
+
+  private failureThresholdPercentage: number;
+
+  private resetTimeout: number;
+
+  constructor(config: CircuitBreakerConfig) {
+    this.failureThresholdPercentage = config.failureThresholdPercentage;
+    this.resetTimeout = config.resetTimeout;
+  }
+
+  get state(): CircuitBreakerState {
+    return this._state;
+  }
+
+  public isClosed(): boolean {
+    return this.state === 'CLOSED';
+  }
+
+  public isOpen(): boolean {
+    return this.state === 'OPEN';
+  }
+
+  public overrideState(state: CircuitBreakerState): void {
+    this._state = state;
+  };
 
   public async run<T>(
-    primaryFn: () => Promise<T>,
-    fallbackFn?: () => Promise<T>,
+    primaryFn: GenericFunction<T>,
+    fallbackFn?: GenericFunction<T>,
   ): Promise<T> {
+    if (this.isOpen()) {
+      if (fallbackFn) {
+        return fallbackFn();
+      }
+      throw new Error('Circuit is open and no fallback provided');
+    }
+
     try {
       const data = await primaryFn();
+      this.onSuccess();
       return data;
     } catch (err) {
+      this.onFailure();
       if (fallbackFn) {
         return fallbackFn();
       }
       throw err;
     }
+  }
+
+  private onSuccess(): void {
+    this.totalCount++;
+  }
+
+  private onFailure(): void {
+    this.totalCount++;
+    this.failureCount++;
+    if (this.isThresholdReached()) {
+      this.open();
+      setTimeout(() => this.reset(), this.resetTimeout);
+    }
+  }
+
+  private isThresholdReached(): boolean {
+    return (this.failureCount / this.totalCount) > (this.failureThresholdPercentage / 100);
+  }
+
+  private reset(): void {
+    this.failureCount = 0;
+    this.totalCount = 0;
+    this.close();
+  }
+
+  private open(): void {
+    this.overrideState('OPEN');
+  }
+
+  private close(): void {
+    this.overrideState('CLOSED');
   }
 }
